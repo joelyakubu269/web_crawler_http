eff96388cc9cb5daa660c7fbc2200854bfa1b3e9
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-23 22:47:29 +0100

    creating a little database using the structs i defined

---
+++ go.mod
@@ -0,0 +1,3 @@
+module queryexercise
+
+go 1.25.4
--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+var users = map[string]User{
+	"1":{
+		ID: "1",
+		Name: "john",
+		Mentions:{
+			"You were mentioned in a post",
+			"Someone tagged you in a comment",
+		},
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	id := query.Get("id")
