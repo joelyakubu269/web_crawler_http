389905fded4210032270284686a42f0b3f6cca20
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-23 23:24:00 +0100

    finished 90 % of the web server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -8,11 +9,19 @@ var users = map[string]User{
 	"1":{
 		ID: "1",
 		Name: "john",
-		Mentions:{
+		Mentions:[]string{
 			"You were mentioned in a post",
 			"Someone tagged you in a comment",
 		},
-	}
+	},
+	"2":{
+		ID: "1",
+		Name: "bukowski",
+		Mentions: []string{
+			"you were mentioned in a post",
+			"someone tagged you in a comment",
+		},
+	},
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,4 +33,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "user not found", http.StatusNotFound)
 		return
 	}
+	w.Header().Set("Content-Type","application/json")
+	switch section {
+	case "mentions":
+		json.NewEncoder(w).Encode(user.Mentions)
+	default:
+		json.NewEncoder(w).Encode(user)
+	}
+}
+func main() {
+	http.HandleFunc("/profile",handler)
+	http.ListenAndServe("8080",nil)
 }
